aaf052423d5ca8b6d5cc6e69d9e435a30de20e4e
Salem <salemmakansi17@gmail.com>
2024-04-14 18:00:24 +0200

    Add files via upload

---
+++ js/verbos.js
@@ -0,0 +1,136 @@
+const is = document.getElementById("is")
+const input= document.getElementById("input-respuesta")
+const paginacion=document.getElementById("paginacion")
+const btn =document.getElementById("btn")
+const container= document.getElementById("container")
+
+let respuestas=[]
+let indice=0
+let contador=1
+let aciertos=0
+let nombre="salem"
+
+let respuestas_correctas=[
+    "gustar",
+    "acabar",
+    "conducir",
+    "oir",
+    "decir",
+    "bañar",
+    "merecer",
+    "desayunar",
+    "costo",
+    "traer",
+    "alcanzar",
+    "adivinar",
+    "apoyar",
+    "rendirse",
+    "firmar"
+    
+]
+
+let palabras=[
+    "like",
+    "finish",
+    "drive",
+    "hear",
+    "say",
+    "take a bath",
+    "deserve",
+    "breakfast",
+    "cost",
+    "bring",
+    "reach",
+    "guess",
+    "support",
+    "give up",
+    "sign"
+
+]
+
+
+function procesar(){
+    if(input.value==""){
+        alert("llena el formulario")
+        
+    }else{
+        respuestas.push(input.value.toLowerCase())
+        input.value=""
+        contador++
+        indice++
+    
+        if(indice+1 > palabras.length){
+            comparar()
+
+        }else{
+            paginacion.innerText= contador+" / "+respuestas_correctas.length
+            is.innerHTML= `<span class="resaltar">${palabras[indice].toUpperCase()}</span>` + " is?"
+    }}
+
+
+function comparar(){
+    for (let i = 0; i < respuestas.length; i++) {
+        if (respuestas[i]==respuestas_correctas[i]) {
+            aciertos++
+        }}
+
+    alert("Has acertado: "+aciertos+ " / "+ respuestas.length)
+    while (container.firstChild){
+        container.removeChild(container.firstChild)
+    }
+
+    mostrarEstadisticas()
+    }
+    
+}
+
+function mostrarEstadisticas(){
+
+
+    container.innerHTML+="<h1>Resultados:</h1>"
+    container.innerHTML+='<table id="tabla"></table>'
+    const tabla=document.getElementById("tabla")
+    tabla.innerHTML+='<tr id="tabla_mostrar"></tr>'
+    const tr=document.getElementById("tabla_mostrar")
+
+    for (let i = 0; i < respuestas_correctas.length; i++) {
+        if (respuestas[i]==respuestas_correctas[i]){
+            tabla.innerHTML+=`<tr><td class="respuestas_correctas">${palabras[i].toUpperCase()}<td class="respuestas_correctas">${respuestas[i]}<td></td></tr>`
+        }else{
+            tabla.innerHTML+=`<tr><td class="respuestas_incorrectas">${palabras[i].toUpperCase()}<td class="respuestas_incorrectas">${respuestas[i]}<td></td></tr>`
+        }
+
+        
+    }
+
+    container.innerHTML+=`<span class="total_aciertos">${aciertos}/${respuestas_correctas.length}<span>`
+    container.innerHTML+="<a href='adjetivos.html'><button>Siguiente</button></a>"
+    toastr.success(`hola ${nombre}! Se han subido los resultados al registro`,"Sistema",{
+        "progressBar": true,
+        "positionClass": "toast-bottom-right",
+        "timeOut": "5000",
+        "extendedTimeOut": "5000",
+        "closeButton": true
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+input.addEventListener("keypress",(e)=>{
+    if(e.key==="Enter"){
+        procesar()
+    }
+})
+btn.addEventListener("click",procesar)
+
+
+is.innerHTML= `<span class="resaltar">${palabras[indice].toUpperCase()}</span>` + " is?"
+paginacion.innerText= contador+" / "+ respuestas_correctas.length
